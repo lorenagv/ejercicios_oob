11caacce0ce4daab26f77a69051a057ca0b02f3c
logavi <logavi20@gmail.com>
2019-02-15 16:46:04 +0100

    ejercicio 2

---
+++ alumno.py
@@ -0,0 +1,20 @@
+
+
+class Alumno():
+    def __init__(self, n_matricula, nombre, nota1,nota2,nota3):
+        self.n_matricula = n_matricula
+        self.nombre = nombre
+        self.nota1 = nota1
+        self.nota2 = nota2
+        self.nota3 = nota3
+
+    def nota_final(self):
+        nota_final = (self.nota1 + self.nota2 + self.nota3) / 3
+        if nota_final >= 4.8:
+            print("El alumno", self.n_matricula, self.nombre," ha aprobado, con nota", nota_final)
+
+        else:
+            print("El alumno", self.n_matricula, self.nombre," ha suspendido, con nota", nota_final)
+
+a = Alumno(188,"Lorena",9,8,9)
+print(a.nota_final())
