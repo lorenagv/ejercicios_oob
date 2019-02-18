7f63bc786c17b7c9c1f2bf912af50ac1ee7c70a4
lorenagv <46898756+lorenagv@users.noreply.github.com>
2019-02-18 10:08:29 +0100

    Update alumno.py

--- alumno.py
+++ alumno.py
@@ -11,10 +11,14 @@ class Alumno():
     def nota_final(self):
         nota_final = (self.nota1 + self.nota2 + self.nota3) / 3
         if nota_final >= 4.8:
-            print("El alumno", self.n_matricula, self.nombre," ha aprobado, con nota", nota_final)
+            print("El alumno", self.n_matricula, self.nombre," ha aprobado")
+            final = "nota final: "+ str(nota_final)
+            return final 
 
         else:
-            print("El alumno", self.n_matricula, self.nombre," ha suspendido, con nota", nota_final)
+            print("El alumno", self.n_matricula, self.nombre,"ha suspendido, con nota", nota_final)
+            final = "nota final: "+ str(nota_final)
+            return final 
 
 a = Alumno(188,"Lorena",9,8,9)
 print(a.nota_final())
